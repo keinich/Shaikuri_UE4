d5d05f7881fc18a4db3fc89444b9dc11fb98771e
keinich <rafael.weschle@gmail.com>
2020-05-06 17:29:40 +0200

    Battle angefangen

---
+++ Source/Shaikuri/Battle/Battle.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Battle.h"
+
+#pragma region Engine Callbacks
+
+// Sets default values
+ABattle::ABattle() {
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABattle::BeginPlay() {
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void ABattle::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+
+}
+
+#pragma endregion
+
+void ABattle::Start(TArray<UFighterComponent*> fighters) {
+	ShowUi();
+}
+
---
+++ Source/Shaikuri/Battle/Battle.h
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+// Engine Includes
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+// Game Includes
+
+// Last Include
+#include "Battle.generated.h"
+
+// Forward Declarations
+class UFighterComponent;
+
+UCLASS()
+class SHAIKURI_API ABattle : public AActor {
+  GENERATED_BODY()
+
+#pragma region Engine Callbacks
+
+public:
+  // Sets default values for this actor's properties
+  ABattle();
+
+  // Called every frame
+  virtual void Tick(float DeltaTime) override;
+
+protected:
+  // Called when the game starts or when spawned
+  virtual void BeginPlay() override;
+
+#pragma endregion
+
+#pragma region Components
+
+
+
+#pragma endregion
+
+public: // UFunctions
+
+  UFUNCTION(BlueprintCallable, category = "Battle")
+    void Start(TArray<UFighterComponent*> fighters);
+
+public: // UProperties
+
+protected: // UFunctions
+
+  UFUNCTION(BlueprintImplementableEvent, category = "Battle")
+    void ShowUi();
+
+protected: // UProperties
+
+protected: // Functions
+
+protected: // Fields
+
+private: // Functions
+
+private: // Fields
+
+  TArray<UFighterComponent*> _Fighters;
+
+};
