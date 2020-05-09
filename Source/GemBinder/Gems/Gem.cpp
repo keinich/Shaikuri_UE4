a5a93625153441995a8b29fce98fffc9bc9c892c
keinich <rafael.weschle@gmail.com>
2020-05-09 20:07:20 +0200

    Gems

--- Source/GemBinder/Gems/Gem.cpp
+++ Source/GemBinder/Gems/Gem.cpp
@@ -3,25 +3,40 @@
 
 #include "Gem.h"
 
+#pragma region Engine Callbacks
+
 // Sets default values
-AGem::AGem()
-{
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+AGem::AGem() {
+  // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+  PrimaryActorTick.bCanEverTick = true;
+
+  Capsule = CreateDefaultSubobject<UCapsuleComponent>("Capsule");
+  Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+  GemNameWidget = CreateDefaultSubobject<UWidgetComponent>("Gem Name Widget");
+  GemImageWidget = CreateDefaultSubobject<UWidgetComponent>("Gem Image Widget");
+  GemDescriptionWidget = CreateDefaultSubobject<UWidgetComponent>("Gem Description Widget");
+
+  SetRootComponent(Capsule);
+  Mesh->SetupAttachment(Capsule);
+  GemNameWidget->SetupAttachment(Capsule);
+  GemImageWidget->SetupAttachment(Capsule);
+  GemDescriptionWidget->SetupAttachment(Capsule);
 
 }
 
 // Called when the game starts or when spawned
-void AGem::BeginPlay()
-{
-	Super::BeginPlay();
-	
+void AGem::BeginPlay() {
+  Super::BeginPlay();
+
 }
 
 // Called every frame
-void AGem::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
+void AGem::Tick(float DeltaTime) {
+  Super::Tick(DeltaTime);
 
 }
+
+#pragma endregion
+
+
 
--- Source/GemBinder/Gems/Gem.h
+++ Source/GemBinder/Gems/Gem.h
@@ -1,26 +1,75 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
+// Engine Includes
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/WidgetComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+// Game Includes
+
+// Last Include
+
+// Forward Declarations
+
 #include "Gem.generated.h"
 
 UCLASS()
-class SHAIKURI_API AGem : public AActor
-{
-	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	AGem();
+class SHAIKURI_API AGem : public AActor {
+  GENERATED_BODY()
+
+#pragma region Engine Callbacks
+
+public:
+  // Sets default values for this actor's properties
+  AGem();
+
+  // Called every frame
+  virtual void Tick(float DeltaTime) override;
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+  // Called when the game starts or when spawned
+  virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+#pragma endregion
+
+#pragma region Components
+
+public:
+
+  UPROPERTY(EditDefaultsOnly)
+    UCapsuleComponent* Capsule;
+
+  UPROPERTY(EditDefaultsOnly)
+    UStaticMeshComponent* Mesh;
+
+  UPROPERTY(EditDefaultsOnly, category = "Gem")
+    UWidgetComponent* GemNameWidget;
+
+  UPROPERTY(EditDefaultsOnly, category = "Gem")
+    UWidgetComponent* GemImageWidget;
+
+  UPROPERTY(EditDefaultsOnly, category = "Gem")
+    UWidgetComponent* GemDescriptionWidget;
+
+#pragma endregion
+
+public: // UFunctions
+
+public: // UProperties
+
+protected: // UFunctions
+
+protected: // UProperties
+
+protected: // Functions
+
+protected: // Fields
+
+private: // Functions
+
+private: // Fields
 
 };
